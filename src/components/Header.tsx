038539dd188f86b4882378be4c4c54b262ddcdef
JobelleCueme <jobellecueme.devsys96@gmail.com>
2025-08-16 06:41:52 +0000

    Updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -28,7 +28,7 @@ const Header = () => {
       <nav className="container mx-auto px-6 py-4 flex justify-between items-center">        
         <div className="flex items-center space-x-3">
           <img 
-            src={`${import.meta.env.BASE_URL}/logo.png`} // replace with your logo path
+            src={`${import.meta.env.BASE_URL}/logo12.png`} // replace with your logo path
             alt="Logo" 
             className="w-20 h-20 object-contain"
           />
